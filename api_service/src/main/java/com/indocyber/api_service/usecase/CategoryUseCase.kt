669036e263bfe3ddcfe611a15e3736c37d624e4a
9 Muhammad Ovie Raka Siwi <muhammadovierakasiwi@gmail.com>
2022-06-13 11:17:03 +0700

    Typo rev

--- api_service/src/main/java/com/indocyber/api_service/usecase/CategoryUseCase.kt
+++ api_service/src/main/java/com/indocyber/api_service/usecase/CategoryUseCase.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.flow.flow
 
 class CategoryUseCase {
     operator fun invoke() = flow {
-        val list = arrayListOf("Business", "Entertaiment", "General", "Health", "Science", "Sports",
+        val list = arrayListOf("Business", "Entertainment", "General", "Health", "Science", "Sports",
             "Technology")
         emit(list)
     }
